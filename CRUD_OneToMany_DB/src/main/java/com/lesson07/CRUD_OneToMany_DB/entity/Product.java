5264b325d8e9634f4e8f4feefc74c5ba81652709
Lê Hoài Khiêm <101650006+lkhiem23@users.noreply.github.com>
2025-03-15 23:28:24 +0700

    Update Product.java

--- CRUD_OneToMany_DB/src/main/java/com/lesson07/CRUD_OneToMany_DB/entity/Product.java
+++ CRUD_OneToMany_DB/src/main/java/com/lesson07/CRUD_OneToMany_DB/entity/Product.java
@@ -1,4 +1,28 @@
 package com.lesson07.CRUD_OneToMany_DB.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Entity
+@Table(name="products")
+@Builder
+@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String productName;
+    String imageUrl;
+    Integer quantity;
+    Double price;
+    Boolean status;
+    @ManyToOne
+    @JoinColumn(name = "categoryId", nullable = false)
+    Category category;
 }
