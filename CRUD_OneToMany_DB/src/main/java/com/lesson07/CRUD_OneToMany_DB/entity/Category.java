94474f8ba289723181385e79261c49fc466a95a2
Lê Hoài Khiêm <101650006+lkhiem23@users.noreply.github.com>
2025-03-15 23:28:10 +0700

    Update Category.java

--- CRUD_OneToMany_DB/src/main/java/com/lesson07/CRUD_OneToMany_DB/entity/Category.java
+++ CRUD_OneToMany_DB/src/main/java/com/lesson07/CRUD_OneToMany_DB/entity/Category.java
@@ -1,4 +1,22 @@
 package com.lesson07.CRUD_OneToMany_DB.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Entity
+@Table(name = "categories")
+@Data
+@Builder
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String CategoryName;
+    Boolean CategoryStatus;
 }
