75bdcb400ca85cdb68ede81f11a41dd15f9f4048
Lê Hoài Khiêm <101650006+lkhiem23@users.noreply.github.com>
2025-03-16 02:45:31 +0700

    Update CategoryService.java

--- CRUD_OneToMany_DB/src/main/java/com/lesson07/CRUD_OneToMany_DB/service/CategoryService.java
+++ CRUD_OneToMany_DB/src/main/java/com/lesson07/CRUD_OneToMany_DB/service/CategoryService.java
@@ -1,4 +1,41 @@
-package com.lesson07.CRUD_OneToMany_DB.service;
+package com.devmaster.lesson07.service;
 
+import com.devmaster.lesson07.entity.Category;
+import com.devmaster.lesson07.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class CategoryService {
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    public CategoryService(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    //Lấy danh sách
+    public List<Category> getAllCategories() {
+        List<Category> categories = categoryRepository.findAll();
+        System.out.println(categories);
+        return categories;
+    }
+
+    // Lấy Category theo id
+    public Optional<Category> getCategoryById(Long id) {
+        return categoryRepository.findById(id);
+    }
+
+    // Cập nhật dữ liệu bảng category: create / update
+    public Category saveCategory(Category category) {
+        return categoryRepository.save(category);
+    }
+
+    // Xóa category theo id
+    public void deleteCategoryById(Long id) {
+        categoryRepository.deleteById(id);
+    }
 }
