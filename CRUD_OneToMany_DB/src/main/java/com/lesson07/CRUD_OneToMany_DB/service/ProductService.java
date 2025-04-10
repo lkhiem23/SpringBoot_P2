a223e71e9cb9c0c863e6049f3e3e88ab50582818
Lê Hoài Khiêm <101650006+lkhiem23@users.noreply.github.com>
2025-04-10 23:58:06 +0700

    Update ProductService.java

--- CRUD_OneToMany_DB/src/main/java/com/lesson07/CRUD_OneToMany_DB/service/ProductService.java
+++ CRUD_OneToMany_DB/src/main/java/com/lesson07/CRUD_OneToMany_DB/service/ProductService.java
@@ -1,4 +1,30 @@
 package com.lesson07.CRUD_OneToMany_DB.service;
 
+@Service
 public class ProductService {
+  @Autowired
+    private ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    //Lấy danh sách
+    public List<Product> getAllProducts() {
+        List<Product> products = productRepository.findAll();
+        System.out.println(categories);
+        return categories;
+    }
+
+    public Optional<Category> getProductById(Long id) {
+        return categoryRepository.findById(id);
+    }
+
+    public Category saveProduct(Product product) {
+        return productRepository.save(product);
+    }
+
+    public void deleteProductById(Long id) {
+        productRepository.deleteById(id);
+    }
 }
